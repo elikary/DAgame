14c9985b9be4d1d598fdf9128debaf1e3fae8343
jzaldumbide <jzaldumbide@gmail.com>
2014-05-22 15:47:23 +1000

    basic connection implemented

--- game/StockMarket/Stock Market/src/main/java/da/stockmarket/stockmarket/LandView.java
+++ game/StockMarket/Stock Market/src/main/java/da/stockmarket/stockmarket/LandView.java
@@ -2,12 +2,22 @@ package da.stockmarket.stockmarket;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import damulticast.Device;
+
+
+import org.apache.http.client.ClientProtocolException;
+
+import java.io.IOException;
+
+import damulticast.Device;
 
 
 public class LandView extends Activity {
@@ -18,7 +28,44 @@ protected static LandFactoryHandler lfHandler;
         setContentView(R.layout.activity_land_view);
         lfHandler = new LandFactoryHandler((ImageButton)this.findViewById(R.id.leftFactory), (ImageButton)this.findViewById(R.id.rightFactory));
         FactorySet.populate();
+        //create connection
+        new Connection().execute();//to create a connection with the tracker
+
+        //end of create connection
+
+
     }
+
+
+    private class Connection extends AsyncTask {
+
+        @Override
+        protected Object doInBackground(Object... arg0) {
+            connect();
+            return null;
+        }
+
+    }
+
+    private void connect() {
+        try {
+            Device device = new Device();
+            String serverIP="192.168.56.1";
+            //String serverIP="10.9.145.111";//running a tracker in that localhost(this ip address is recognized by genymotion)
+            device.establishConnection(serverIP);
+
+        } catch (ClientProtocolException e) {
+            Log.d("HTTPCLIENT", e.getLocalizedMessage());
+        } catch (IOException e) {
+            Log.d("HTTPCLIENT", e.getLocalizedMessage());
+        }
+    }
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
